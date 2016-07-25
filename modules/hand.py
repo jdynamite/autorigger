e6d4ecb556416660222a4d4697f01e731f492ef1
Daniel Arata <d.arata@rocketmail.com>
2016-07-24 21:02:02 -0500

    friendly push

--- modules/hand.py
+++ modules/hand.py
@@ -1,199 +1,238 @@
-'''
-Hand Class
-'''
+#Spline Class
 
 import maya.cmds as cmds
-
 from autorigger.lib import nameSpace
 from autorigger.lib import control
 from autorigger.lib import joint
 from autorigger.lib import locator
-from autorigger.lib import attribute
-
-from autorigger.modules import part
-from autorigger.modules import finger
 
 
+class Spline(object) :
 
-#limb inherits a bit from IkfkLimb
-class Hand(part.Part):
+    def __init__( self, name, startJointDupe, endJointDupe, jointAmmount=7,
+                  controlAmmount=3 ):
 
-    def __init__(self,
-                 name,
-                 position = (12,10,0.25),
-                 fingerCount=5,
-                 fingerPositions=([0.5,0,0.05],[0,0,0.5],[0,0,0],[0,0,-0.5],[0,0,-1]) ):
-        super(Hand,self).__init__(name)
-        self.position = position
-        self.fingerCount = fingerCount
-        self.fingerPositions = fingerPositions
-        self.guides = list()
-        #I don't ever expect to go over 5, but just in case here is 7
-        #we can always add more if needed
-        fingerNameIterator = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+        self.name = name
+        self.jointAmmount = jointAmmount
+        self.controlAmmount = controlAmmount
+        self.startJointDupe = startJointDupe
+        self.endJointDupe = endJointDupe
 
-        self.fingerRig = list()
-        for i in range(0, self.fingerCount):
-            #store finger name in variable
-            replaceHand = self.getName().replace('hand', 'finger')
-            fingerName = "{0}{1}".format( replaceHand, fingerNameIterator[i] )
+        self.jointList = list()
+        self.curve = "{0}_curveIk".format( self.name )
 
-            if i==0:
-                #thumb would only have a finger count of 4
-                self.fingerRig.append(finger.Finger(fingerName,
-                                                        jointCount=4,
-                                                        positions=(
-                                                            [1,0,0],
-                                                            [1.5,0,0],
-                                                            [2,0,0],
-                                                            [2.5,0,0] )))
-            else:
-                #store class in list
-                self.fingerRig.append( finger.Finger(fingerName) )
+    def getStartCtrl(self):
+        return self.startCtrl.getName()
 
+    def getMidCtrl(self):
+        return self.midCtrl.getName()
+
+    def getEndCtrl(self):
+        return self.endCtrl.getName()
+
+    def getStartCtrlZero(self):
+        return self.startCtrl.getZeroGroup1()
+
+    def getMidCtrlZero(self):
+        return self.midCtrl.getZeroGroup1()
+
+    def getEndCtrlZero(self):
+        return self.endCtrl.getZeroGroup1()
+
+    def getSplineIk(self):
+        return self.ik
+
+    def getSplineCurve(self):
+        return self.curve
+
+    def getJoint(self):
+        return self.jointList
+
+    def setWorldUpObjectStart(self, influence):
+        cmds.connectAttr( "{0}.worldMatrix[0]".format(influence), '{0}.dWorldUpMatrix'.format( self.getSplineIk() ) )
+
+    def setWorldUpObjectEnd(self, influence):
+        cmds.connectAttr( "{0}.worldMatrix[0]".format(influence), '{0}.dWorldUpMatrixEnd'.format(self.getSplineIk() ) )
 
     def setup(self):
-        super(Hand,self).setup()
 
-        #store masterGuide in self.guides
-        self.guides.append( self.masterGuide )
-
-        '''
-        if self.getSide() == nameSpace.SIDES["right"]:
-            self.position = (-12,10,0.25)
-            self.fingerPositions = ([-0.5,0,0.05],[0,0,0.5],[0,0,0],[0,0,-0.5],[0,0,-1])
-        '''
-
-        #add aim and up attr to masterGuide
-        self.aimAttr = attribute.switch(self.masterGuide.getName(),
-                        "aim",0,
-                        ["x","y","z","-x","-y","-z"],
-                        [[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]])
-
-        self.upAttr = attribute.switch(self.masterGuide.getName(),
-                        "up",0,
-                        ["x","y","z","-x","-y","-z"],
-                        [[1,0,0],[0,1,0],[0,0,1],[-1,0,0],[0,-1,0],[0,0,-1]])
-
-        self.masterGuide.setPosition( self.position )
-        for index,finger in enumerate(self.fingerRig):
-            #create all the finger setups
-            finger.setup()
-            #place the master guides
-            finger.masterGuide.setPosition( self.fingerPositions[index] )
-            #parent constraint them to masterGuides
-            cmds.parentConstraint( self.masterGuide.getName(), finger.setupGroup)
-            #connect aim and up from hand master guide to each finger master guide
-            cmds.connectAttr( "{0}.aim".format( self.masterGuide.getName() ),
-                              "{0}.aim".format( finger.masterGuide.getName() ) )
-            cmds.connectAttr( "{0}.up".format( self.masterGuide.getName() ),
-                              "{0}.up".format( finger.masterGuide.getName() ) )
-
-            #hide the attributes on the fingers
-            cmds.setAttr( "{0}.aim".format(finger.masterGuide.getName()),
-                          lock=True, keyable=False, channelBox=False)
-            cmds.setAttr( "{0}.up".format(finger.masterGuide.getName()),
-                          lock=True, keyable=False, channelBox=False)
-            
-
-            #And finally, append finger guide to self.guide
-            for guides in finger.guides:
-
-                self.guides.append( guides )
+        #create a 2 joint chain so that we have them pointing correctly
+        #duplicate start joint
+        startJoint = cmds.duplicate( self.startJointDupe, po=True, n='startJoint' )[0]
+        startJointPosition = cmds.xform( startJoint, q=True, ws=True, t=True )
 
 
-        #rotate thumb (fingerA) to point outwards
-        cmds.select(self.fingerRig[0].masterGuide.getName())
-        cmds.rotate(0,-90,0,r=True)
+        endJoint = cmds.duplicate( self.endJointDupe, po=True, n='endJoint' )[0]
+        endJointPosition = cmds.xform( endJoint, q=True, ws=True, t=True )
 
-        #position hand master guide
-        cmds.select( "{0}.cv[*]".format(self.masterGuide.getName()) )
-        cmds.scale(1.5,1.5,1.5,r=True)
-        cmds.move(0,0.5,0,r=True)
-        cmds.select(cl=True)
-
-        #set it's color
-        self.masterGuide.setColor(17)
-        self.masterGuide.setPosition( self.position )
-
-        print '{0}.up'.format(self.masterGuide.getName())
-        cmds.setAttr( '{0}.up'.format(self.masterGuide.getName()), 1 )
-
-    def postSetup(self):
-        #super(Hand,self).postSetup()
-        #run postSetup on each finger
-
-        for finger in self.fingerRig:
-            finger.postSetup()
-
-    def preBuild(self):
-        #super(Hand,self).preBuild()
-        #run preBuild on each finger
-
-        for finger in self.fingerRig:
-            finger.preBuild()
-
-        cmds.delete(self.setupGroup)
-
-    def build(self):
-        super(Hand,self).build()
-
-        cmds.createNode("transform", n=self.group)
-        cmds.createNode("transform", n=self.hookGroup)
-        cmds.parent(self.hookGroup, self.group)
-
-        #run build on each finger
-        for index,finger in enumerate(self.fingerRig):
-
-            finger.build()
-
-            #parent all to hook
-            cmds.parent(finger.group, self.hookGroup)
+        cmds.parent(endJoint, startJoint)
 
 
-        # Connections
-        #fingerC1 and D1 rotate at a percentage of fingerE1
-        fingerC1Mdn = cmds.createNode('multiplyDivide', n="{0}_mdn".format(self.fingerRig[2].getName() ))
-        fingerD1Mdn = cmds.createNode('multiplyDivide', n="{0}_mdn".format(self.fingerRig[3].getName() ))
-        #set fingerC1Mdn.input2 to 0.50
-        cmds.setAttr( "{0}.input2X".format(fingerC1Mdn), 0.25 )
-        cmds.setAttr( "{0}.input2Y".format(fingerC1Mdn), 0.25 )
-        cmds.setAttr( "{0}.input2Z".format(fingerC1Mdn), 0.25 )
-        #set fingerD1Mdn.input2 to 0.25
-        cmds.setAttr( "{0}.input2X".format(fingerD1Mdn), 0.5 )
-        cmds.setAttr( "{0}.input2Y".format(fingerD1Mdn), 0.5 )
-        cmds.setAttr( "{0}.input2Z".format(fingerD1Mdn), 0.5 )
-        #connect fingerE to mdn input1x
-        cmds.connectAttr( "{0}.rotate".format(self.fingerRig[4].getfkChainControlName(0)), "{0}.input1".format(fingerC1Mdn) )
-        cmds.connectAttr( "{0}.rotate".format(self.fingerRig[4].getfkChainControlName(0)), "{0}.input1".format(fingerD1Mdn) )
-        #now connect mdn output to fingerc and d
-        cmds.connectAttr( "{0}.output".format(fingerC1Mdn), "{0}.rotate".format(self.fingerRig[2].getfkChainControlName(0)) )
-        cmds.connectAttr( "{0}.output".format(fingerD1Mdn), "{0}.rotate".format(self.fingerRig[3].getfkChainControlName(0)) )
-
-        #now hide and lock the finger controls we won't need
-        cmds.select( self.fingerRig[1].getfkChainControlName(0),
-                     self.fingerRig[2].getfkChainControlName(0),
-                     self.fingerRig[3].getfkChainControlName(0) )
-        cmds.pickWalk(d='down')
-        cmds.delete()
-
-        #make the pinky hand ctrl a little bigger
-        cmds.select("{0}.cv[*]".format(self.fingerRig[4].getfkChainControlName(0)))
-        cmds.scale(2,2,2,r=True)
-        cmds.select(cl=True)
+        #create joint chain
+        for i in range( 0, self.jointAmmount ):
+            jointNum = i+1
+            self.jointList.append( cmds.duplicate( startJoint,
+                                                   n= '{0}_{1}{2}'.format( self.name, animName.SPLINE, str(jointNum) ),
+                                                   po=True )[0] )
+            #place them accordingly
+            #get tx attr
+            totalDist = cmds.getAttr( '{0}.tx'.format( endJoint ))
+            iteration = totalDist/ (self.jointAmmount-1)
+            cmds.setAttr('{0}.radius'.format(self.jointList[i]), 2.25)
+            if i>0:
+                cmds.parent( self.jointList[i], self.jointList[i-1] )
+                cmds.setAttr( '{0}.tx'.format(self.jointList[i]), iteration)
+                cmds.setAttr( '{0}.ty'.format(self.jointList[i]), 0)
+                cmds.setAttr( '{0}.tz'.format(self.jointList[i]), 0)
 
 
-        # clean up: hierarchy
+        #determine joint chain down axis will always be x
 
 
-    def postBuild(self):
-        super(Hand,self).postBuild()
-        #run postBuild on each finger
-        for index,finger in enumerate(self.fingerRig):
+        #create curve
+        loc = cmds.spaceLocator(n='inbetweenSpaceLoc')[0]
+        cmds.parentConstraint( startJoint, endJoint, loc )
+        halfway = cmds.getAttr('{0}.translate'.format(loc))
+        print halfway
 
-            finger.postBuild()
 
 
+        points = [
+
+            startJointPosition,
+            cmds.xform( loc, q=1, ws=1, t=1),
+            endJointPosition
+
+        ]
+        cmds.curve(ep=points, d=3, n=self.curve )
+
+        #delete the start and end joints, we don't need them anymore
+        cmds.delete( startJoint, endJoint )
+        #delete the loc
+        cmds.delete(loc)
+
+        #Create the spline IKH
+        self.ik = cmds.ikHandle( sol="ikSplineSolver",
+                            sj=self.jointList[0],
+                            ee=self.jointList[-1],
+                            ccv = False,
+                            c = self.curve,
+                            n ="{0}_splineIk".format(self.name) )[0]
+        cmds.setAttr('{0}.dTwistControlEnable'.format(self.ik), 1 )
+        cmds.setAttr('{0}.dWorldUpType'.format(self.ik), 4 )
+        cmds.setAttr('{0}.v'.format(self.ik), 0 )
+
+    #add stretch to joints
+        #create curveInfo node
+        cmds.select( self.curve )
+        arc = cmds.arclen( ch=True )
+        arclen = '{0}_arcLength'.format(self.name)
+        cmds.rename( arc, arclen )
+
+        #create mdn
+        mdn = cmds.createNode( 'multiplyDivide', n='{0}_mdn'.format(self.name) )
+        #set mdn to divide
+        cmds.setAttr('{0}.operation'.format(mdn), 2)
+        #connect curveInfo.arcLength to mdn.input1X and mdn.input2X
+        cmds.connectAttr( '{0}.arcLength'.format(arclen), '{0}.input1X'.format(mdn) )
+        #query distance
+        dist = cmds.getAttr( '{0}.arcLength'.format(arclen) )
+        #set distance to input2
+        cmds.setAttr( '{0}.input2X'.format(mdn), dist )
+
+        #connect to all joints
+        for jnt in self.jointList:
+
+            cmds.connectAttr( '{0}.outputX'.format(mdn), '{0}.sx'.format(jnt) )
+
+
+
+
+    def controlUp(self):
+
+        self.startCtrl = control.Control(
+            'start_{0}_{1}{2}'.format(
+                self.name,
+                nameSpace.SPLINE,
+                nameSpace.CONTROL),
+                position = cmds.xform(self.jointList[0], q=1, ws=1, t=1),
+                shape='cross'
+        )
+
+        self.startAim = locator.Locator(
+            'start_{0}_{1}'.format(self.name, nameSpace.LOCATOR)
+        )
+
+        self.midCtrl = control.Control(
+            'mid_{0}_{1}{2}'.format(
+                self.name,
+                nameSpace.SPLINE,
+                nameSpace.CONTROL
+            ),
+            shape='star')
+
+        self.endCtrl = control.Control(
+            'end_{0}_{1}{2}'.format(
+                self.name,
+                nameSpace.SPLINE,
+                nameSpace.CONTROL),
+                position = cmds.xform( self.jointList[-1], q=1, ws=1, t=1),
+                shape='cross')
+
+        self.endAim = locator.Locator( 'end_{0}_{1}'.format(self.name, nameSpace.LOCATOR))
+
+    #start control
+        self.startCtrl.create()
+        self.startAim.create()
+        self.startAim.setParent( self.startCtrl.getName() )
+        #snap..
+        self.startAim.setPosition( cmds.xform( self.jointList[0], q=1, ws=1, t=1))
+
+
+    #mid control
+        self.midCtrl.create()
+        self.midCtrl.setOrientation('x')
+    #end control
+        self.endCtrl.create()
+        self.endAim.create()
+        self.endAim.setParent( self.endCtrl.getName() )
+        #snap..
+        self.endAim.setPosition( cmds.xform( self.jointList[-1], q=1, ws=1, t=1) )
+
+        #pointConstrain start and end to mid
+        cmds.pointConstraint( self.startCtrl.getName(), self.endCtrl.getName(), self.midCtrl.getZeroGroup1() )
+        #aim mid to end ZeroGroup1
+
+        #cmds.aimConstraint( self.endCtrl.getName(), self.midCtrl.getZeroGroup1(),
+        #                    aim = [1,0,0], u = [0,1,0], wut = 'objectrotation', wuo= self.startCtrl.getName() )
+
+    #aim the start and end locators to mid
+        cmds.aimConstraint( self.midCtrl.getName(), self.startAim.getName(),
+                            aim = [1,0,0], u = [0,1,0], wut = 'objectrotation', wuo= self.startCtrl.getZeroGroup1() )
+        cmds.aimConstraint( self.midCtrl.getName(), self.endAim.getName(),
+                            aim = [-1,0,0], u = [0,1,0], wut = 'objectrotation', wuo= self.endCtrl.getZeroGroup1() )
+    #clusters...
+        #startClusters
+        cmds.select( '{0}.cv[0:1]'.format(self.getSplineCurve()) )
+        startCls = cmds.cluster(envelope=True, n='{0}_startCls'.format(self.name) )
+
+        #midClusters
+        cmds.select( '{0}.cv[2]'.format(self.getSplineCurve()) )
+        midCls = cmds.cluster(envelope=True, n='{0}_midCls'.format(self.name) )
+
+        #endClusters
+        cmds.select( '{0}.cv[3:4]'.format(self.getSplineCurve()) )
+        endCls = cmds.cluster(envelope=True, n='{0}_endCls'.format(self.name) )
+
+    #parent those clusters!
+        cmds.parent( startCls, self.startAim.getName() )
+        cmds.parent( midCls, self.midCtrl.getName() )
+        cmds.parent( endCls, self.endAim.getName() )
+
+        #hide clusters
+        cmds.setAttr( '{0}.v'.format(startCls[1]), 0 )
+        cmds.setAttr( '{0}.v'.format(midCls[1]), 0 )
+        cmds.setAttr( '{0}.v'.format(endCls[1]), 0 )
 
 
 
